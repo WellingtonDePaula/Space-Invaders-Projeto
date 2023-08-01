67631da38d449c560503d0f6dcbc36fbc125bb26
WellingtonDePaula <wellingtonespiao@gmail.com>
2023-08-01 14:47:15 -0300

    começando projeto SPACE INVADERS

---
+++ naveAtora.js
@@ -0,0 +1,49 @@
+var xNave = 697.5;
+var yNave = 620;
+var velocidadeXNave = 6;
+var tiroChamado = false;
+var velocidadeTiro = 25;
+var xTiro = [xNave+17, xNave+17];
+var yTiro = [yNave-50, yNave-50];
+var tiroSaiuDaNave = false;
+
+function desenhaNave() {
+    fill(0);
+    rect(xNave, yNave, 50, 80);
+}
+
+function movimentaNave() {
+    if(keyIsDown(LEFT_ARROW)) {
+        xNave -= velocidadeXNave;
+    }
+    if(keyIsDown(RIGHT_ARROW)) {
+        xNave += velocidadeXNave;
+    }
+}
+
+function criaTiro() {
+    for(let i = 0; i < 2; i ++) {
+        if(tiroChamado === true) {
+            fill(255);
+            rect(xTiro[i], yTiro[i], 15, 35);
+            yTiro[i] -= velocidadeTiro;
+            tiroSaiuDaNave = true;
+        }
+    }
+    for(let i = 0; i < 2; i++) {
+        if(yTiro[i] <= -50) {
+            yTiro[i] = yNave-50; tiroChamado = false;
+        }
+    }
+}
+function verificaSeOTiroSaiuDaTela() {
+    if(tiroSaiuDaNave === true) {
+        xTiro[0] = xNave+17;
+    }
+}
+
+function keyTyped() {
+    if(key === "z") {
+        tiroChamado = true;
+    }
+}
---
+++ script.js
@@ -0,0 +1,19 @@
+var alturaDaTela = innerHeight;
+var larguraDaTela = innerWidth;
+function setup() {
+    createCanvas(larguraDaTela, alturaDaTela);
+  }
+  
+  function draw() {
+    background(15);
+    desenhaCenario();
+    desenhaNave();
+    movimentaNave();
+    criaTiro();
+    verificaSeOTiroSaiuDaTela();
+  }
+
+  function desenhaCenario() {
+    fill(30);
+    rect(200, 0, 1095, alturaDaTela);
+  }
